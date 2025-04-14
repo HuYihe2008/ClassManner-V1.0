f0cb6b5802829f51734a88d5a47031145e1ec491
HuYihe2008 <2812856215@qq.com>
2025-04-15 02:11:00 +0800

    🚀feat: 更新依赖项，添加加载页面，优化 Electron 主窗口创建逻辑

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,8 @@ declare module 'vue' {
   export interface GlobalComponents {
     Breadcrumb: typeof import('./src/components/Breadcrumb.vue')['default']
     ElAvatar: typeof import('element-plus/es')['ElAvatar']
+    ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
+    ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
     ElCarousel: typeof import('element-plus/es')['ElCarousel']
